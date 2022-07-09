9323dee179ac8f75796112a3b41534f8a6ba4c0c
浩会海 <50604324+haohuihai@users.noreply.github.com>
2022-07-09 13:53:10 +0800

    111

--- webhook.js
+++ webhook.js
@@ -26,6 +26,7 @@ let server = http.createServer(function(req,res){
       res.end(JSON.stringify({"ok": true}))
       if(event === 'push'){
         let payload = JSON.parse(body);
+        console.log('payload',payload)
         let child = spawn('sh', [`./${payload.repository.name}.sh`]);
         let buffers = [];
         child.stdout.on('data', function (buffer) { buffers.push(buffer)});
