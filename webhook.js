a5111e7a0a102777a0b1f191e276248e44ce4ed8
浩会海 <50604324+haohuihai@users.noreply.github.com>
2022-07-09 10:08:01 +0800

    1111

--- webhook.js
+++ webhook.js
@@ -1,7 +1,7 @@
 let http = require('http')
 let crypto = require('crypto');
-// var spawn = require('child_precess').spawn;
-// let sendMail = require('./sendMail');
+var spawn = require('child_precess').spawn;
+let sendMail = require('./sendMail');
 
 const SECRET = '123456';
 function sign(data) {
@@ -25,7 +25,20 @@ let server = http.createServer(function(req,res){
       res.setHeader('Content-Type','application/json');
       res.end(JSON.stringify({"ok": true}))
       if(event === 'push'){
-        console.log('body',body)
+        let payload = JSON.parse(body);
+        let child = spawn('sh', [`./${payload.repository.name}.sh`]);
+        let buffers = [];
+        child.stdout.on('data', function (buffer) { buffers.push(buffer)});
+        child.stdout.on('end', function () {
+          let logs = Buffer.concat(buffers).toString();
+          sendMail(`
+            <h1>部署日期: ${new Date()}</h1>
+            <h2>部署人: ${payload.pusher.name}</h2>
+            <h2>部署邮箱: ${payload.pusher.email}</h2>
+            <h2>提交信息: ${payload.head_commit&&payload.head_commit['message']}</h2>
+            <h2>布署日志: ${logs.replace("\r\n",'<br/>')}</h2>
+        `);
+        });
       }
     })
 
